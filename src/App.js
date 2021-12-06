3dff0cdfa14802eb7e5f8a1d961f33950176c0cf
codingSlate <ananta2021k@gmail.com>
2021-12-06 19:09:09 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import './style.css';
 import Counter from './Counter';
 import Random from './Random';
+import './style.css';
 
 export default function App() {
   return (
     <div>
       <h3>Counter with useState</h3>
       <Counter />
-      {/* <Random /> */}
+      <Random />
     </div>
   );
 }
