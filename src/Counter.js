38912e895bf8615d480e556ed1a734b013851775
codingSlate <ananta2021k@gmail.com>
2021-12-06 20:57:07 +0530

    1 file modified

--- src/Counter.js
+++ src/Counter.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import InteractiveView from './InteractiveView';
 export default function Counter() {
   const [count, setCount] = useState(0);
-  const onIncrementHandler = () => setCount(count + 1);
+  const onIncrementHandler = () => setCount((c) => c + 1);
   return (
     <InteractiveView
       value={count}
